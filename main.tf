b388cce238e057e7476d8c6f5798b2df70fe79a2
Rafal Wilinski <raf.wilinski@gmail.com>
2017-03-16 23:15:35 +0100

    Add custom beanstalk instance profile

--- main.tf
+++ main.tf
@@ -15,11 +15,76 @@ resource "aws_ecr_repository" "ng_container_repository" {
   name = "${var.application_name}"
 }
 
+# Beanstalk instance profile
+resource "aws_iam_instance_profile" "ng_beanstalk_ec2" {
+  name  = "beanstalk-ec2-user"
+  roles = ["${aws_iam_role.beanstalk_ec2.name}"]
+}
+
+resource "aws_iam_role" "ng_beanstalk_ec2" {
+  name = "ng-beanstalk-ec2-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+# Beanstalk EC2 Policy
+# Overriding because by default Beanstalk does not have a permission to Read ECR
+resource "aws_iam_role_policy" "ng_beanstalk_ec2_policy" {
+  name = "ng_beanstalk_ec2_policy_with_ECR"
+  role = "${aws_iam_role.test_role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "cloudwatch:PutMetricData",
+        "ds:CreateComputer",
+        "ds:DescribeDirectories",
+        "ec2:DescribeInstanceStatus",
+        "logs:*",
+        "ssm:*",
+        "ec2messages:*",
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchCheckLayerAvailability",
+        "ecr:GetDownloadUrlForLayer",
+        "ecr:GetRepositoryPolicy",
+        "ecr:DescribeRepositories",
+        "ecr:ListImages",
+        "ecr:DescribeImages",
+        "ecr:BatchGetImage",
+        "s3:*",
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+# Beanstalk Application
 resource "aws_elastic_beanstalk_application" "ng_beanstalk_application" {
   name        = "${var.application_name}"
   description = "${var.application_description}"
 }
 
+# Beanstalk Environment
 resource "aws_elastic_beanstalk_environment" "ng_beanstalk_application_environment" {
   name                = "${var.application_name}-${var.application_environment}"
   application         = "${aws_elastic_beanstalk_application.ng_beanstalk_application.name}"
@@ -29,12 +94,16 @@ resource "aws_elastic_beanstalk_environment" "ng_beanstalk_application_environme
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "InstanceType"
-    value     = "t2.micro"
+
+    # Todo: As Variable
+    value = "t2.micro"
   }
 
   setting {
     namespace = "aws:autoscaling:asg"
     name      = "MaxSize"
-    value     = "2"
+
+    # Todo: As Variable
+    value = "2"
   }
 }
